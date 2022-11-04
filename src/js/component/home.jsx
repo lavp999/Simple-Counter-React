6ff484ed0ebbd6ccf30a85ec0d57af624ba4d648
Luis <lavp99@hotmail.com>
2022-11-04 23:55:46 +0100

    nada, no hay manera de que saque nas de un cuadro

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,19 +5,19 @@ import Caja from "../component/caja.js";
 
 const Home = (props) => {
 	return (
-		<>
+		<><h1>hola</h1>
 			<div className="miFila">
-			<div className="cajaSimbolo">
-            	    <h2 className="text-center">wq{props.contador}</h2>
+				<div className="cajaSimbolo">
+            	    <h2 className="text-center"><i class="fas fa-clock"></i></h2>
         	    </div>
 				<div className="cajaNumero">
-            	    <h2 className="text-center">wq{props.contador}</h2>
+            	    <h2 className="text-center">65{props.contador}</h2>
         	    </div>
 				<div className="cajaNumero">
-            	    <h2 className="text-center">wq{props.contador}</h2>
+            	    <h2 className="text-center">876{props.contador}</h2>
         	    </div>
 				<div className="cajaNumero">
-            	    <h2 className="text-center">wq{props.contador}</h2>
+            	    <h2 className="text-center">87{props.contador}</h2>
         	    </div>
 			</div>
 		</>
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,12 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
 
 import "../styles/index.css";               // include your styles into the webpack bundle
 import Home from "./component/home.jsx";    //import your own components
 
 let miContador = 0;
 setInterval(() => {
-    ReactDOM.render(<Home contador="{miContador}" />, document.querySelector("#app"));
+    ReactDOM.render(<Home contador={miContador} />, document.querySelector("#app"));
     miContador += 1;
 }, 1000);
 
