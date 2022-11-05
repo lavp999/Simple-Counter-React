6e647907327cd6b4d0d0b26e2f7443af9eba6338
Luis <lavp99@hotmail.com>
2022-11-05 08:58:13 +0100

    con cadenas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,18 +4,19 @@ import Caja from "../component/caja.js";
 // <Caja numero = {props.contador} caja = "simbolo" />
 
 const Home = (props) => {
-	const miNumero = props.contador;
+	const miNumero = props.contador.toString().padStart(7,"0") + "."; // no lo covertia en cadena
+	
 	return (
 		<>
 			<div className="miFila">
 				<Caja numero="" caja="simbolo"/>
-				<Caja numero={Math.floor(miNumero / 1000000)} caja="numero"/>
-				<Caja numero={Math.floor(miNumero / 100000)} caja="numero"/>
-				<Caja numero={Math.floor(miNumero / 10000)} caja="numero"/>
-				<Caja numero={Math.floor(miNumero / 1000)} caja="numero"/>
-				<Caja numero={Math.floor(miNumero / 100)} caja="numero"/>
-				<Caja numero={Math.floor(miNumero / 10)} caja="numero"/>
-				<Caja numero={Math.floor(miNumero)} caja="numero"/>
+				<Caja numero={miNumero.slice(0,1)} caja="numero"/>
+				<Caja numero={miNumero.slice(1,2)} caja="numero"/>
+				<Caja numero={miNumero.slice(2,3)} caja="numero"/>
+				<Caja numero={miNumero.slice(3,4)} caja="numero"/>
+				<Caja numero={miNumero.slice(4,5)} caja="numero"/>
+				<Caja numero={miNumero.slice(5,6)} caja="numero"/>
+				<Caja numero={miNumero.slice(6,7)} caja="numero"/>
 			</div>
 		</>
 	);
