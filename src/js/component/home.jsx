a7f72668ee7e7389c8174032e9322a4cd9f897ad
Luis <lavp99@hotmail.com>
2022-11-05 09:56:28 +0100

    alguna cosilla mas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,9 +5,25 @@ import Caja from "../component/caja.js";
 
 const Home = (props) => {
 	const miNumero = props.contador.toString().padStart(7,"0") + "."; // no lo covertia en cadena
+	const ctaAtras = props.cuentaAtras.toString().padStart(7,"0") + "."; // no lo covertia en cadena
+	
+	function hemosLlegado(){
+		if(props.contador == props.avisarEn) alert("Hemos llegado a " + props.avisarEn);
+	}
+
+	function iniciar(){  // esto no se puede hacer
+		props.contador = 0;
+		props.cuentaAtras = 10000000;
+	}
+	
+	function parada(){
+	}
+	function reanudar(){
+	}
 	
 	return (
 		<>
+			{hemosLlegado()}
 			<div className="miFila">
 				<Caja numero="" caja="simbolo"/>
 				<Caja numero={miNumero.slice(0,1)} caja="numero"/>
@@ -17,6 +33,23 @@ const Home = (props) => {
 				<Caja numero={miNumero.slice(4,5)} caja="numero"/>
 				<Caja numero={miNumero.slice(5,6)} caja="numero"/>
 				<Caja numero={miNumero.slice(6,7)} caja="numero"/>
+			</div>
+
+			<div className="miFila">
+				<Caja numero="" caja="simbolo"/>
+				<Caja numero={ctaAtras.slice(0,1)} caja="numero"/>
+				<Caja numero={ctaAtras.slice(1,2)} caja="numero"/>
+				<Caja numero={ctaAtras.slice(2,3)} caja="numero"/>
+				<Caja numero={ctaAtras.slice(3,4)} caja="numero"/>
+				<Caja numero={ctaAtras.slice(4,5)} caja="numero"/>
+				<Caja numero={ctaAtras.slice(5,6)} caja="numero"/>
+				<Caja numero={ctaAtras.slice(6,7)} caja="numero"/>
+			</div>
+
+			<div className="miFila" role="toolbar" aria-label="Toolbar with button groups">
+				<button type="button" className="cajaSimbolo" onClick={iniciar}><i className="fas fa-clock"></i></button>
+				<button type="button" className="cajaSimbolo" onClick={parada}><i class="fas fa-stopwatch"></i></button>
+				<button type="button" className="cajaSimbolo" onClick={reanudar}><i className="fas fa-clock"></i></button>
 			</div>
 		</>
 	);
--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,14 @@ import "../styles/index.css";               // include your styles into the webp
 import Home from "./component/home.jsx";    //import your own components
 
 let miContador = 0;
+let cuentaAtras = 10000000;
 setInterval(() => {
-    ReactDOM.render(<Home contador={miContador} />, document.querySelector("#app"));
+    ReactDOM.render(<Home contador={miContador} 
+                          cuentaAtras={cuentaAtras} 
+                          avisarEn="10"
+    />, document.querySelector("#app"));
+
     miContador += 1;
+    cuentaAtras--;
 }, 1000);
 
