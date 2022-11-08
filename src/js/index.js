0cc82b8ea31345d868560339e910405c398ef7b1
lavp999 <lavp99@hotmail.com>
2022-11-08 02:05:14 +0000

    simplificado pero con cadenas

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,16 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";               // include your styles into the webpack bundle
 import Home from "./component/home.jsx";    //import your own components
 
+const cajas = 5
 let miContador = 0;
-let cuentaAtras = 10000000;
+let cuentaAtras = parseInt("9".padStart(cajas,"9"), 10);
+
+
 setInterval(() => {
-    ReactDOM.render(<Home contador={miContador} 
-                          cuentaAtras={cuentaAtras} 
-                          avisarEn="10"
+    ReactDOM.render(<Home contador={miContador.toString().padStart(cajas,"0") } 
+                          cuentaAtras={cuentaAtras.toString().padStart(cajas,"9") } 
+                          avisarEn={10}
+                          cajas={cajas}
     />, document.querySelector("#app"));
 
     miContador += 1;
